c63abc8a3cb03189e7923e041f230c861cd5389b
leekarmun <146315088+leekarmun@users.noreply.github.com>
2023-09-28 13:36:59 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+<script type="text/javascript">
+var counter = 1;
+setInterval(function(){
+  document.getElementById('radio' + counter).checked = true;
+  counter++;
+  if(counter > 5){
+     counter = 1;
+  }
+}, 6000);
+</script>
